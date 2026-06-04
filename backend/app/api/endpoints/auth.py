65e2b5aebb01d4ad7acfbc34893b9ddbee69eadd
Oleg <oleg_malhas@mail.ru>
2026-06-04 15:31:59 +0200

    feat: add token after register

--- backend/app/api/endpoints/auth.py
+++ backend/app/api/endpoints/auth.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends, HTTPException, status, Response
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 
 from backend.app.core.security import create_access_token
-from backend.app.schemas.auth import TokenResponse
+from backend.app.schemas.auth import TokenResponse, AuthResponse
 from backend.app.core.config import settings
 from backend.app.dependencies.auth import get_auth_service, get_current_user
 from backend.app.schemas.user import UserResponse, CreateUser
@@ -22,12 +22,29 @@ router = APIRouter(prefix="/auth", tags=["auth"])
     status_code=status.HTTP_201_CREATED,
     response_model=UserResponse,
 )
+@router.post(
+    "/register/",
+    status_code=status.HTTP_201_CREATED,
+    response_model=AuthResponse,
+)
 async def register(
         data: CreateUser,
         auth_service: Annotated[AuthService, Depends(get_auth_service)],
 ):
     user = await auth_service.register(data)
-    return user
+
+    access_token = create_access_token(
+        user_id=user.id,
+        username=user.username,
+        is_admin=user.is_admin,
+    )
+
+    return {
+        "user": user,
+        "access_token": access_token,
+        "is_admin": user.is_admin,
+        "token_type": "bearer"
+    }
 
 
 @router.post(
@@ -50,8 +67,10 @@ async def login_user(
     )
 
     return {
+        "user": user,
         "access_token": access_token,
-        "token_type": "bearer",
+        "is_admin": user.is_admin,
+        "token_type": "bearer"
     }
 
 
--- backend/app/schemas/auth.py
+++ backend/app/schemas/auth.py
@@ -1,5 +1,7 @@
 from pydantic import BaseModel
 
+from backend.app.schemas.user import UserResponse
+
 
 class LoginRequest(BaseModel):
     username: str
@@ -7,5 +9,11 @@ class LoginRequest(BaseModel):
 
 
 class TokenResponse(BaseModel):
+    access_token: str
+    token_type: str = "bearer"
+
+
+class AuthResponse(BaseModel):
+    user: UserResponse
     access_token: str
     token_type: str = "bearer"
